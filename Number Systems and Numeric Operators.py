93fc30e738ced4a523ac59b15ca43807a279b93e
Candace Hollinger <csig1985@gmail.com>
2022-12-10 04:27:37 +0000

    update Number System and Numeric Operators

---
+++ Number Systems and Numeric Operators.py
@@ -0,0 +1,31 @@
+#Addition with integers
+print(2 + 2)
+Ouput = 4
+
+#Addition with a float
+print(2.0 + 2.0)
+Output = 4
+
+#Multiplication with integers
+print(3 * 9)
+Ouput = 27
+
+#Division with integers
+print(5 / 3)
+Output = 1.6666666666666667
+
+#Floor Division with integers
+print(5 // 3)
+Output = 1
+
+#Floor Division with a whole number remainder
+print(5 % 3)
+Output = 2
+
+#Exponents
+print(2 ** 3)
+Ouput = 8
+
+
+
+
